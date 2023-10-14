81fdf5b892a910f58a4b147cc4edf60acf9b660a
Tonyliu2004 <ltsplice@gmail.com>
2023-10-14 19:42:10 -0400

    Did API calls, displays, and title search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,116 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Card from './Components/card.jsx';
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
+  const [foodData, setfoodData] = useState(null);
+  const [totalRecipes, setTotalRecipes] = useState(0);
+  const [averageHealthScore, setAverageHealthScore] = useState(0);
+  const [averageCalories, setAverageCalories] = useState(0);
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      const response = await fetch(
+          `https://api.spoonacular.com/recipes/complexSearch?cuisine=chinese&apiKey=${API_KEY}&number=10&addRecipeInformation=true&addRecipeNutrition=true` 
+        );
+      const json = await response.json();
+      setfoodData(json);
+      setTotalRecipes(json.totalResults);
+      console.log(json); //log
+      let healthScore = 0;
+      let avgCalories=0;
+      for(let i = 0;i < json.number;i++){
+        healthScore+=json.results[i].healthScore;
+        avgCalories+=json.results[i].nutrition.nutrients[0].amount;
+      }
+      setAverageHealthScore(healthScore/json.number);
+      setAverageCalories((avgCalories/json.number).toFixed(2));
+    };
+    fetchFoodData().catch(console);
+  }, []);
 
+
+  //searches
+  const searchFoods = searchFood => {
+    setSearchFoodInput(searchFood);
+    if(searchFood != ""){
+      const filtered = Object.entries(foodData.results).filter((item)=>
+        Object.values(item[1].title)
+        .join("")
+        .toLowerCase()
+        .includes(searchFood.toLowerCase())
+      )
+      setFilteredResults(filtered);
+    }
+  }
+
+  const searchIngredients = searchIngredient => {
+    console.log("INGREDIENT: " + searchIngredient);
+  }
+
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchFoodInput, setSearchFoodInput] = useState("");
   return (
-    <div>
-      hi
+    <div className = "the-world">
+      <h1>Amazing Recipes!</h1>
+      <div className = "stats-container">
+        <div className= "stat-card">
+          <h3>Total Number of Recipes</h3>
+          <h4>{totalRecipes}</h4>
+        </div>
+        <div className= "stat-card">
+          <h3>Average Health Score</h3>
+          <h4>{averageHealthScore}</h4>
+        </div>
+        <div className= "stat-card">
+          <h3>Average Calories:</h3>
+          <h4>{averageCalories}</h4>
+        </div>
+      </div>
+      
+      <div className = "search-bars">
+        <input
+          type="text"
+          placeholder="Foods"
+          onChange={(inputString) => searchFoods(inputString.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Ingredients"
+          onChange={(inputString) => searchIngredients(inputString.target.value)}
+        />
+      </div>
+{/* testing cards
+      <Card image = "https://spoonacular.com/recipeImages/646425-312x231.jpg" ingredients = {["cookie","bum"]} name = "Fried rice" calories = "10" healthScore={100}/>
+      <Card image = "https://spoonacular.com/recipeImages/663169-312x231.jpg" ingredients = {["cookie","bum"]} name = "Random Salad" calories = "10" healthScore={100}/>
+      <Card image = "https://spoonacular.com/recipeImages/638642-312x231.jpg" ingredients = {["cookie","bum"]} name = "Bum Salad" calories = "10" healthScore={100}/>
+      <Card image = "https://spoonacular.com/recipeImages/660231-312x231.jpg" ingredients = {["cookie","bum"]} name = "Veggie Fried Rice" calories = "10" healthScore={100}/> */}
+
+      {searchFoodInput.length > 0 ? 
+      filteredResults.map(([dish]) =>
+       <Card image={foodData.results[dish].image} 
+             ingredients={foodData.results[dish].nutrition.ingredients} 
+             name={foodData.results[dish].title} 
+             calories={foodData.results[dish].nutrition.nutrients[0].amount} 
+             healthScore={foodData.results[dish].healthScore}/> 
+        )
+        : foodData && Object.entries(foodData.results).map(([dish]) =>
+        <Card image={foodData.results[dish].image} 
+              ingredients={foodData.results[dish].nutrition.ingredients} 
+              name={foodData.results[dish].title} 
+              calories={foodData.results[dish].nutrition.nutrients[0].amount} 
+              healthScore={foodData.results[dish].healthScore}/>
+        )
+      }
+
+
+      {/* {foodData && Object.entries(foodData.results).map(([dish]) =>
+       <Card image={foodData.results[dish].image} 
+             ingredients={foodData.results[dish].nutrition.ingredients} 
+             name={foodData.results[dish].title} 
+             calories={foodData.results[dish].nutrition.nutrients[0].amount} 
+             healthScore={foodData.results[dish].healthScore}/>
+      )} */}
     </div>
   )
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,20 @@
+import React, {useState } from "react";
+import './card.css'
+
+const Card = ({name, image, ingredients, calories, healthScore}) => {
+    return(
+        <div className = "container">
+            <img className = "recipe-image" src = {image} alt = {`Image: ${image}`}/>
+            <div className="recipe-details">
+                <h2 className= "recipe-name">{name}</h2>
+                <div className="recipe-parts">
+                    <h4>Ingredients: {ingredients.map(item => item.name).join(', ')}</h4>
+                    <h4>Calories: {calories}</h4>
+                    <h4>Health Score: {healthScore}</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
